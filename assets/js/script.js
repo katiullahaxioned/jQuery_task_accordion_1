705df3f9b198d39b29ac01d6ecba15b05ab364ef
katiullahaxioned <atiullahk@axioned>
2022-09-02 19:19:39 +0530

    initial functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,13 @@
-/* Author: 
+$(document).ready(function () {
+  var openClose = $(".open-close-box");
 
-*/
+  openClose.first().siblings("p").slideDown();
+  openClose.first().children().addClass('active');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  openClose.click(function () {
+    openClose.siblings("p").slideUp();
+    $(this).siblings("p").slideToggle();
+    openClose.children().removeClass('active');
+    $(this).children().addClass('active');
+  });
+});
